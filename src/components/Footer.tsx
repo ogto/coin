a51084bd5bae9c1fd07002d74b1d64e92744d055
ogto <ogto@kakao.com>
2025-12-18 15:19:35 +0900

    [fix]

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -102,7 +102,7 @@ export default function Footer() {
         {/* 회사 정보 + 카피라이트 */}
         <div className="grid gap-4 py-8 md:grid-cols-2">
           <div className="text-xs leading-6 text-white/60">
-            <div>상호: 해강 주식회사 / Hae Gang Co, Ltd. | 대표: 이서우 | 사업자등록번호: 110-86-19547</div>
+            <div>상호: 해강홀딩스 주식회사 / Hae Gang Co, Ltd. | 대표: 이서우 | 사업자등록번호: 110-86-19547</div>
             <div>주소: 서울특별시 강남구 도곡로 7길, 5층 | Email. info@Bunnystock.io</div>
           </div>
           <div className="flex items-end justify-start md:justify-end">
